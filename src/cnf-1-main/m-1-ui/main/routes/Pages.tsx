ca46d28bb8b6b55afc33b78be515f4aee2e03a17
ai73a@yandex.ru <1qazxcvBG>
2020-08-18 15:18:43 +0300

    add main page redirect

--- src/cnf-1-main/m-1-ui/main/routes/Pages.tsx
+++ src/cnf-1-main/m-1-ui/main/routes/Pages.tsx
@@ -3,6 +3,7 @@ import LoginPage from "../../../../cnf-2-fatures/f-1-auth/a-1-login/l-1-ui/Login
 import RegisterPage from "../../../../cnf-2-fatures/f-1-auth/a-2-register/r-1-ui/RegisterPage";
 import ProfilePage from "../../../../cnf-2-fatures/f-1-auth/a-7-profile/p-1-ui/ProfilePage";
 import AuthRedirectPage from "../../../../cnf-0-common/c-1-ui/redirect/AuthRedirectPage";
+import { Redirect } from "react-router-dom";
 // import PacksPage from "../../../../cnf-2-fatures/f-2-cards/c-1-packs/p-1-ui/PacksPage";
 // import CardsPage from "../../../../cnf-2-fatures/f-2-cards/c-2-cards/c-1-ui/CardsPage";
 // import LearnPage from "../../../../cnf-2-fatures/f-3-learn/l-1-ui/LearnPage";
@@ -28,20 +29,21 @@ export const PATH = {
 };
 
 export const pages: PageType[] = [
-    {_id: 0, title: "login", path: PATH.LOGIN, exact: true, page: <LoginPage/>},
-    {_id: 1, title: "register", path: PATH.REGISTER, exact: true, page: <RegisterPage/>},
+    {_id: 0, title: "main", path: "/", exact: true, page: <Redirect to={PATH.LOGIN}/>},
+    {_id: 1, title: "login", path: PATH.LOGIN, exact: true, page: <LoginPage/>},
+    {_id: 2, title: "register", path: PATH.REGISTER, exact: true, page: <RegisterPage/>},
     // {
-    //     id: 2, title: 'profile', path: PATH.PROFILE, params: '/:id', exact: true,
+    //     id: 3, title: 'profile', path: PATH.PROFILE, params: '/:id', exact: true,
     //     page: (p) => <div>{p.match.params.id}</div>
     // },
     {
-        _id: 2, title: "profile", path: PATH.PROFILE, exact: true,
+        _id: 3, title: "profile", path: PATH.PROFILE, exact: true,
         page: <AuthRedirectPage><ProfilePage/></AuthRedirectPage>
     },
-    // {id: 3, title: 'packs', path: PATH.PACKS, exact: true, page: () => <PacksPage/>},
-    // {id: 4, title: 'cards', path: PATH.CARDS, params: '/:id', exact: true, page: () => <CardsPage/>},
-    // {id: 5, title: 'learn', path: PATH.LEARN, params: '/:id', exact: true, page: () => <LearnPage/>},
-    // {id: 6, title: 'test', path: PATH.TEST, exact: true, page: () => <TestPage/>},
+    // {id: 4, title: 'packs', path: PATH.PACKS, exact: true, page: () => <PacksPage/>},
+    // {id: 5, title: 'cards', path: PATH.CARDS, params: '/:id', exact: true, page: () => <CardsPage/>},
+    // {id: 6, title: 'learn', path: PATH.LEARN, params: '/:id', exact: true, page: () => <LearnPage/>},
+    // {id: 7, title: 'test', path: PATH.TEST, exact: true, page: () => <TestPage/>},
 
     {_id: 9999, title: "error404", page: <div>error404</div>}
 ];
